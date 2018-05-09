05b2681c49fa710daaaa889bed21edb8f55f59c0
Alberto Chiusole <bebo.sudo@gmail.com>
2018-05-10 00:53:09 +0200

    Setup logging system (needs some improvements).

--- poppy/file_parser.py
+++ poppy/file_parser.py
@@ -1,10 +1,23 @@
+import logging
+
 import yaml
+
+LOGGER = logging.getLogger(__name__)
 
 
 def yaml_string_to_dict_converter(yaml_string):
+    LOGGER.debug("Converting YAML string '%s' to a python dict.",
+                 yaml_string[:50].replace("\n", "\\n"))
     return yaml.load(yaml_string)
 
 
 def filename_to_dict_converter(filename):
-    with open(filename) as input_fd:
-        return yaml_string_to_dict_converter(input_fd.read())
+    LOGGER.debug("Going to open file '%s' in order to convert it to a python dict.", filename)
+    try:
+        with open(filename) as input_fd:
+            LOGGER.debug("File '%s' opened, going to convert it to a python dict.", filename)
+            return yaml_string_to_dict_converter(input_fd.read())
+
+    except FileNotFoundError as e:
+        LOGGER.error("Unable to find file '%s' to be converted to a python dict.", filename)
+        raise
--- tests/test_yaml_converter.py
+++ tests/test_yaml_converter.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-
 import unittest
-import io
 import os.path
 from tempfile import TemporaryDirectory
 
@@ -13,30 +11,45 @@ class YAMLTester(unittest.TestCase):
     """Test that the YAML converter meets the YAML standard."""
 
     def setUp(self):
-        self.example_input_file = ("Species:\n  - x\n  - y\n  - z\n\nObservables:\n"
+        self.example_input_text = ("Species:\n  - x\n  - y\n  - z\n\nObservables:\n"
                                    "  - x_tot = x + y\n\nReactions:\n  - 3x + y = z\n")
-        # Simulate the behaviour of a file from a string.
-        self.fake_input_file = io.StringIO(self.example_input_file)
-        self.expected_dictionary = {"Species": ["x", "y", "z"],
-                                    "Observables": ["x_tot = x + y"],
-                                    "Reactions": ["3x + y = z"]}
+        self.expected_converted_dictionary = {"Species": ["x", "y", "z"],
+                                              "Observables": ["x_tot = x + y"],
+                                              "Reactions": ["3x + y = z"]}
 
     def test_interpret_file_from_yaml_to_dict(self):
-        self.assertTrue(self.expected_dictionary ==
-                        poppy.file_parser.yaml_string_to_dict_converter(self.fake_input_file))
+        self.assertTrue(self.expected_converted_dictionary ==
+                        poppy.file_parser.yaml_string_to_dict_converter(self.example_input_text))
 
     def test_interpret_empty_file_from_yaml_to_dict(self):
-        empty_input_file = io.StringIO("")
+        empty_input_file = ""
         self.assertIsNone(poppy.file_parser.yaml_string_to_dict_converter(empty_input_file))
 
     def test_filename_to_dict(self):
         with TemporaryDirectory() as tmpdirname:
             temp_filename = os.path.join(tmpdirname, "test_input.txt")
             with open(temp_filename, "w") as temp_fd:
-                temp_fd.write(self.example_input_file)
+                temp_fd.write(self.example_input_text)
 
-            self.assertTrue(self.expected_dictionary ==
+            self.assertTrue(self.expected_converted_dictionary ==
                             poppy.file_parser.filename_to_dict_converter(temp_filename))
+
+    def test_empty_filename(self):
+        with TemporaryDirectory() as tmpdirname:
+            temp_filename = os.path.join(tmpdirname, "test_input.txt")
+            with open(temp_filename, "w") as temp_fd:
+                temp_fd.write("")
+            self.assertIsNone(poppy.file_parser.filename_to_dict_converter(temp_filename))
+
+            with open(temp_filename, "w") as temp_fd:
+                temp_fd.write("\n")
+            self.assertIsNone(poppy.file_parser.filename_to_dict_converter(temp_filename))
+
+    def test_non_existing_filename(self):
+        with TemporaryDirectory() as tmpdirname:
+            temp_filename = os.path.join(tmpdirname, "test_input.txt")
+            with self.assertRaises(FileNotFoundError):
+                poppy.file_parser.filename_to_dict_converter(temp_filename)
 
     def tearDown(self):
         # Here we can place repetitive code that should be performed _after_ every test is executed.
