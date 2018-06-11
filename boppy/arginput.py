cf7f862b640a8c8d35609e79f9249dc9c1661a37
Erika Tomat <erika.317.astrea@gmail.com>
2018-06-11 21:57:37 +0200

    Modified with a function that calls file_parser

--- boppy/arginput.py
+++ boppy/arginput.py
@@ -1,7 +1,15 @@
-import argparse 
+import argparse
+import os.path
+from file_parser import filename_to_dict_converter
+
+def does_file_exist(parser, arg):
+	if not os.path.exists(arg):
+		parser.error("The file %s does not exist!" % arg)
+	else:
+		filename_to_dict_converter(arg)
 
 parser = argparse.ArgumentParser(description="...")
 parser.add_argument("file", help="input file")
 args = parser.parse_args()
 out = args.file
-print(out)
+does_file_exist(parser, out)
