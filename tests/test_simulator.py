ab9319f55714491644499da77dbfec14a249aad2
teresapca <33687057+teresapca@users.noreply.github.com>
2018-06-13 15:14:23 +0200

    Correct test next reaction method

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -55,7 +55,6 @@ class SimulatorsTest(unittest.TestCase):
         self.assertEqual(trajectory_states.shape, exp_trajectory_states.shape)
         self.assertEqual(np.allclose(trajectory_times, exp_trajectory_times), True)
 
-    """
     def test_next_reaction_method(self):
 
         secondary_parameters = {'affects': self.nrm_affects_1, 'depends_on': self.nrm_depends_on_1}
@@ -67,16 +66,20 @@ class SimulatorsTest(unittest.TestCase):
                                                                        **secondary_parameters
                                                                        )
         exp_trajectory_states = np.array([[8, 2, 0], [8, 1, 1], [7, 2, 1], [6, 3, 1], [5, 4, 1],
-                                          [4, 5, 1], [4, 4, 2], [3, 5, 2], [2, 6, 2], [2, 5, 3],
-                                          [2, 4, 4], [2, 3, 5], [1, 4, 5], [0, 5, 5], [0, 4, 6],
-                                          [0, 3, 7], [0, 2, 8], [0, 1, 9], [0, 0, 10]])
+                                          [4, 5, 1], [3, 6, 1], [3, 5, 2], [2, 6, 2], [1, 7, 2],
+                                          [0, 8, 2], [0, 7, 3], [0, 6, 4], [0, 5, 5], [0, 4, 6],
+                                          [0, 3, 7], [0, 2, 8], [1, 2, 7], [0, 3, 7], [0, 2, 8],
+                                          [0, 1, 9], [1, 1, 8], [0, 2, 8], [0, 1, 9], [0, 0, 10],
+                                          [1, 0, 9], [2, 0, 8]])
         exp_trajectory_times = np.array([0.00000000, 0.50541675, 0.72215369,
-                                         2.04913925, 3.08132503, 4.50426089,
-                                         4.22471507, 4.66393921, 4.89407833,
-                                         6.10604296, 6.22825155, 7.14520634,
-                                         9.76701837, 14.0287953, 15.24456935,
-                                         21.19422457, 25.49303166, 33.88760137,
-                                         58.56049933])
+                                         2.04924969, 3.63028953, 3.70212303,
+                                         3.95660752, 4.13785332, 4.15049748,
+                                         4.17595761, 4.43794469, 13.88975128,
+                                         18.76035349, 24.41389058, 29.17361476,
+                                         32.39772008, 37.99409988, 43.38133488,
+                                         45.83765185, 48.83291652, 58.87425443,
+                                         67.59444266, 70.01291698, 72.29467578,
+                                         82.76564785, 85.69228982, 91.22928178])
 
         self.assertEqual(trajectory_states.shape, exp_trajectory_states.shape)
         self.assertEqual(np.allclose(trajectory_states,
@@ -85,7 +88,6 @@ class SimulatorsTest(unittest.TestCase):
         self.assertEqual(trajectory_times.shape, exp_trajectory_times.shape)
         self.assertEqual(np.allclose(trajectory_times,
                                      exp_trajectory_times), True)
-    """
 
     # def test_Gillespie_1(self):
     #     output = boppy.simulators.Gillespie(self.update_matrix_1,
