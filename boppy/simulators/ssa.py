a9881a6326f29ee496a3bf838fa019ae14e69d3c
Erika Tomat <erika.317.astrea@gmail.com>
2018-06-22 12:31:59 +0200

    Time vector as a numpy array

--- boppy/simulators/ssa.py
+++ boppy/simulators/ssa.py
@@ -4,15 +4,14 @@ import numpy as np
 
 
 def SSA(update_matrix, initial_conditions, function_rate, t_max, **kwargs):
-
     """Function that implement the Stochastic Simulation Algorithm"""
-    
+
     def initialize_vector_binary_search(vector):
         """
         Function that generate a vector of lenght 2*m-1, where m is the lenght of the given vector,
         when the element at the position i is the sum of elements of position 2*i and 2*i + 1
                 """
-        binary_vector = np.zeros(shape = len(vector)-1) #total nodes for len(vector) values
+        binary_vector = np.zeros(shape=len(vector)-1)  # total nodes for len(vector) values
         binary_vector = np.append(binary_vector, vector)
         i = len(vector)-2
         while i > -1:
@@ -35,14 +34,14 @@ def SSA(update_matrix, initial_conditions, function_rate, t_max, **kwargs):
                 i = 2*i + 2
             if i >= cut_value:
                 verify = False
-    
+
         return int(i - cut_value)
 
     i = 0
-    #vector of number of reaction for each times
+    # vector of number of reaction for each times
     data_vector = []
     data_vector.append(initial_conditions)
-    #vector of times
+    # vector of times
     time_vector = []
     time_vector.append(0)  # default
     app_t = 0
@@ -51,26 +50,26 @@ def SSA(update_matrix, initial_conditions, function_rate, t_max, **kwargs):
         rate = function_rate(initial_conditions)
         total_rate = sum(rate)
 
-        #Generate the two random number of algorithm: first to choose reaction, second to choose time 
+        # Generate the two random number of algorithm: first to choose reaction, second to choose time
 
-        r1 = np.random.uniform(0.0001, total_rate) 
-        r2 = np.random.uniform(0.0001, 1) 
+        r1 = np.random.uniform(0.0001, total_rate)
+        r2 = np.random.uniform(0.0001, 1)
 
         t = -1.0 / total_rate * np.log(r2)
         app_t = t + time_vector[i]
 
-        #choose reaction and update the vector of reactions
+        # choose reaction and update the vector of reactions
 
         vector_binary = initialize_vector_binary_search(rate)
-        reaction = binary_search_processing(vector_binary,r1)
+        reaction = binary_search_processing(vector_binary, r1)
 
         initial_conditions = initial_conditions + update_matrix[reaction, :]
 
-        #update the data_vector and time_vector
+        # update the data_vector and time_vector
 
         time_vector.append(app_t)
         data_vector.append(initial_conditions)
 
         i += 1
 
-    return np.array(data_vector), time_vector
+    return np.array(data_vector), np.array(time_vector)
